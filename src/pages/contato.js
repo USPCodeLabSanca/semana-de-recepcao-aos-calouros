5a027c3010cd1b4b1a98f49ba68ebf10f5fe28a3
Carolina Arenas <caro.arenas2607@gmail.com>
2020-01-21 13:13:37 -0300

    add maps api

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 const style = {
   root: 'py-10 px-2 md:px-16',
   title: 'text-3xl font-bold',
 }
 
-export default function Contato () {
+export function Contato () {
   return (
     <main className={style.root}>
       <h1 className={style.title}>Serviço de Graduação</h1>
       <p>Bloco 4</p>
       <p>(16) 3373-9639</p>
       <p>grad@icmc.usp.br</p>
+
+      <Map
+          google={window.google}
+          zoom={17}
+          initialCenter={{ lat: -22.0070347, lng: -47.8949323 }}
+        />
     </main>
     
   )
 }
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_MAPS_API_KEY
+})(Contato);
