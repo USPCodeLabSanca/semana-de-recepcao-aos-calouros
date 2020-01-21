5b9c21b61ea1a57b583ddf8cc503b7e9f8d89f58
Carolina Arenas <caro.arenas2607@gmail.com>
2020-01-21 12:39:17 -0300

    add contact info

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+const style = {
+  root: 'py-10 px-2 md:px-16',
+  title: 'text-3xl font-bold',
+}
+
 export default function Contato () {
   return (
-    <h1>This is the 'contato' page! :D</h1>
+    <main className={style.root}>
+      <h1 className={style.title}>Serviço de Graduação</h1>
+      <p>Bloco 4</p>
+      <p>(16) 3373-9639</p>
+      <p>grad@icmc.usp.br</p>
+    </main>
+    
   )
 }
