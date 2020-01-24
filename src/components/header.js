d07161d87540e7c5b0d2b0db3e7864e57d396c10
jean sidharta <jeansidharta@gmail.com>
2020-01-24 00:00:23 -0300

    quick patch; some styling was fixed

--- src/components/header.js
+++ src/components/header.js
@@ -6,11 +6,12 @@ import HeaderBG from '../images/header-bg.jpg'
 import Logo from '../images/logo.png'
 
 const style = {
-  root: 'h-64 overflow-hidden flex flex-col sm:flex-row sm:ml-20 items-center pt-4',
-  top: 'flex items-center pt-6 w-full',
+  root: 'h-64 overflow-hidden pt-4',
+  linkContainer: 'flex flex-col sm:flex-row sm:ml-20 items-center',
+  top: 'flex items-center pt-6 px-4 w-full',
   image: 'w-20 h-20 sm:mr-12 object-contain',
   bgImage: 'absolute t-0 z-1 h-64 w-full object-cover',
-  link: 'sm:mx-4 text-white font-bold text-sm w-1/3 sm:w-auto',
+  link: 'mx-1 sm:mx-4 text-white font-bold text-sm w-1/3 sm:w-auto',
   label: 'text-3xl font-bold text-center my-4 text-white'
 }
 
@@ -31,15 +32,17 @@ function Header () {
       style={{ height: pathname === '/' ? '100vh' : '' }}
     />
     <header className={style.root}>
-      <Link to='/'><img
-        src={Logo}
-        alt="Logo da recepção escrito 'Chega mais'"
-        className={style.image}
-      /></Link>
-      <div className={style.top}>
-        <Link to='/#programacao' className={style.link + ' text-right'}>PROGRAMAÇÃO</Link>
-        <Link to='/disque-trote' className={style.link + ' text-center'}>DISQUE-TROTE</Link>
-        <Link to='/contato' className={style.link + ' text-left'}>CONTATO</Link>
+      <div className={style.linkContainer}>
+        <Link to='/'><img
+          src={Logo}
+          alt="Logo da recepção escrito 'Chega mais'"
+          className={style.image}
+        /></Link>
+        <div className={style.top}>
+          <Link to='/#programacao' className={style.link + ' text-right'}>PROGRAMAÇÃO</Link>
+          <Link to='/disque-trote' className={style.link + ' text-center'}>DISQUE-TROTE</Link>
+          <Link to='/contato' className={style.link + ' text-left'}>CONTATO</Link>
+        </div>
       </div>
       <div className={style.label}>{label}</div>
     </header>
