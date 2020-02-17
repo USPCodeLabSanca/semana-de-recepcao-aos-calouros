9edb228082d9b1489e1e5b94067a7a956a3662c7
Rafael Doering <rafaelsoares@tokenlab.com.br>
2020-02-17 02:40:20 -0300

    remove countdown

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Moment from 'moment'
+// import Moment from 'moment'
 
 const style = {
   root: 'ml-10 sm:ml-20 color-white absolute top-0 h-screen flex flex-col justify-center pointer-events-none',
@@ -10,40 +10,42 @@ const style = {
   countdownContainer: 'block sm:flex items-end'
 }
 
-const Small = ({ ...props }) => <p className={style.smallText} {...props} />
+// const Small = ({ ...props }) => <p className={style.smallText} {...props} />
 
 const Large = ({ ...props }) => <p className={style.largeText} {...props} />
 
-const TimeContainer = ({ ...props }) => <div className={style.timeContainer} {...props} />
+// const TimeContainer = ({ ...props }) => <div className={style.timeContainer} {...props} />
 
-const targetDate = Moment('17/02/2020 - 08h 00m', 'DD/MM/YYYY - HH[h] mm[m]', true)
+// sconst targetDate = Moment('17/02/2020 - 08h 00m', 'DD/MM/YYYY - HH[h] mm[m]', true)
 
-const calculateDiff = () => Moment.duration(Moment(targetDate).diff(Moment()))
+// const calculateDiff = () => Moment.duration(Moment(targetDate).diff(Moment()))
 
 function Countdown () {
-  const [diff, setDiff] = React.useState(calculateDiff())
+  // const [diff, setDiff] = React.useState(calculateDiff())
 
-  React.useEffect(() => {
+  /*React.useEffect(() => {
     const handler = setInterval(() => setDiff(calculateDiff), 100)
     return () => clearInterval(handler)
-  })
+  })*/
 
   return (<>
     <div className={style.root}>
-      <div className={style.countdownContainer}>
-        <TimeContainer>
-          <Large>{diff.days()}</Large><Small>dias</Small>
-        </TimeContainer>
-        <TimeContainer>
-          <Large>{diff.hours()}</Large><Small>horas</Small>
-        </TimeContainer>
-        <TimeContainer>
-          <Large>{diff.minutes()}</Large><Small>minutos</Small>
-        </TimeContainer>
-        <TimeContainer>
-          <Large>{diff.seconds()}</Large><Small>segundos</Small>
-        </TimeContainer>
-      </div>
+      {/*
+        <div className={style.countdownContainer}>
+          <TimeContainer>
+            <Large>{diff.days()}</Large><Small>dias</Small>
+          </TimeContainer>
+          <TimeContainer>
+            <Large>{diff.hours()}</Large><Small>horas</Small>
+          </TimeContainer>
+          <TimeContainer>
+            <Large>{diff.minutes()}</Large><Small>minutos</Small>
+          </TimeContainer>
+          <TimeContainer>
+            <Large>{diff.seconds()}</Large><Small>segundos</Small>
+          </TimeContainer>
+        </div>
+      */}
       <Large>Você faz parte disso tudo!</Large>
       <Large>22ª Semana de Recepção aos Calouros</Large>
     </div>
