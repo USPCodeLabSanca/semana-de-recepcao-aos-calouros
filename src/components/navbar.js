412cfa01ceadf03cfba50d20e4c60de0aec23a8c
RafaelDoering <rafael.doering.soares@usp.br>
2021-03-10 23:41:59 -0300

    Add navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import {Link} from 'react-router-dom';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import Logo from '../images/logo.svg';
+
+/**
+ * Navbar
+ *
+ * @return {void}
+ */
+function Navbar() {
+  const links = [{
+    path: '/programacao',
+    text: 'programação',
+  }, {
+    path: '/disque-trote',
+    text: 'disque-trote',
+  }, {
+    path: '/informacoes',
+    text: 'informações',
+  }, {
+    path: '/contato',
+    text: 'contato',
+  }];
+
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+
+  return (<>
+    <header className='bg-black shadow h-20 fixed top-0 left-0 right-0'>
+      <div className='flex h-full items-center justify-between container mx-auto px-4'>
+        <Link className='h-full' to='/'><img
+          src={Logo}
+          alt="Logo do ICMC"
+          className='h-full py-4'
+        /></Link>
+        <div className='hidden lg:flex'>
+          {links.map((link) => {
+            return (<Link key={link.path} to={link.path} className='color-white p-4'><b>{link.text}</b></Link>);
+          })}
+        </div>
+        <div className='lg:hidden'>
+          <IconButton color='secondary' aria-label='Abrir menu' onClick={() =>setIsDrawerOpen(true)}>
+            <MenuIcon />
+          </IconButton>
+        </div>
+      </div>
+      <SwipeableDrawer
+        anchor='left'
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        onOpen={() => setIsDrawerOpen(true)}
+        PaperProps={{style: {backgroundColor: '#151515'}}}
+      >
+        <div
+          className='flex flex-col items-center pt-4'
+          style={{minWidth: '260px'}}
+        >
+          {links.map((link) => {
+            return (<Link key={link.path} to={link.path} onClick={() => setIsDrawerOpen(false)} className='color-white p-4'><b>{link.text}</b></Link>);
+          })}
+        </div>
+      </SwipeableDrawer>
+    </header>
+  </>);
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
+import {createMuiTheme} from '@material-ui/core/styles';
+import {ThemeProvider} from '@material-ui/styles';
 
 import Home from './pages/home';
 import DisqueTrote from './pages/disque-trote';
 import Contato from './pages/contato';
+import Navbar from './components/navbar';
 
 import './main-style.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e68d13',
+    },
+    secondary: {
+      main: '#fafafa',
+    },
+  },
+});
 
 /**
  * App Component
@@ -15,14 +30,17 @@ import './main-style.css';
  */
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/disque-trote' component={DisqueTrote} />
-        <Route exact path='/contato' component={Contato} />
-        <Redirect to='/' />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/disque-trote' component={DisqueTrote} />
+          <Route exact path='/contato' component={Contato} />
+          <Redirect to='/' />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
