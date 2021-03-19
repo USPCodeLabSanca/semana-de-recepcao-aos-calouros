ed63d13d759e8a905cab774fcc21090b03e18793
RafaelDoering <rafael.doering.soares@usp.br>
2021-03-19 10:33:34 -0300

    Return main page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
+import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
 import {createMuiTheme} from '@material-ui/core/styles';
 import {ThemeProvider} from '@material-ui/styles';
 
-// import Home from './pages/home';
-// import DisqueTrote from './pages/disque-trote';
-// import Contato from './pages/contato';
-// import Navbar from './components/navbar';
-import CallToAction from './components/call-to-action';
+import Home from './pages/home';
+import DisqueTrote from './pages/disque-trote';
+import Contato from './pages/contato';
+import Navbar from './components/navbar';
+// import CallToAction from './components/call-to-action';
 
 import './main-style.css';
 
@@ -31,20 +31,20 @@ const theme = createMuiTheme({
  */
 function App() {
   return (
-  // <ThemeProvider theme={theme}>
-  // <BrowserRouter>
-  // <Navbar />
-  // <Switch>
-  // <Route exact path='/' component={Home} />
-  // <Route exact path='/disque-trote' component={DisqueTrote} />
-  // <Route exact path='/contato' component={Contato} />
-  // <Redirect to='/' />
-  // </Switch>
-  // </BrowserRouter>
-  // </ThemeProvider>
     <ThemeProvider theme={theme}>
-      <CallToAction />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/disque-trote' component={DisqueTrote} />
+          <Route exact path='/contato' component={Contato} />
+          <Redirect to='/' />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <CallToAction />
+    // </ThemeProvider>
   );
 }
 
