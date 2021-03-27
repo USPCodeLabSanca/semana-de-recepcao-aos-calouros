703fdcd77be3e3b8b73dfac0e5ae081140362853
RafaelDoering <rafael.doering.soares@usp.br>
2021-03-27 02:33:34 -0300

    Refactor Foooter

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,5 @@
 import React from 'react';
 
-import Image from '../images/logo.png';
-
-const style = {
-  root: 'bg-black w-full h-48 text-center flex flex-col justify-center items-center',
-  image: 'w-20 h-20 mb-4',
-  link: 'text-blue pl-1 link',
-  text: 'text-xs color-white',
-};
-
 /**
  * Footer
  *
@@ -16,15 +7,8 @@ const style = {
  */
 export default function Footer() {
   return (
-    <div className={style.root}>
-      <img alt='Logo da semana de recepção' src={Image} className={style.image} style={{objectFit: 'contain'}} />
-      <p className={style.text}>
-        Feito com ❤ pelo
-        <a href='http://uclsanca.icmc.usp.br/' rel='noreferrer noopener' className={style.link}>
-          CodeLab
-        </a>
-      </p>
-      <p className={style.text}>
+    <div className='bg-black w-full h-32 text-center flex flex-col justify-center items-center'>
+      <p className='text-xs color-white'>
         ©2020 Instituto de Ciências Matemáticas e de Computação
       </p>
     </div>
