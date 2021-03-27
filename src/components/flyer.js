a918195bb3b03008a8316d56db64c886cfb2a7d4
RafaelDoering <rafael.doering.soares@usp.br>
2021-03-27 02:40:52 -0300

    Refactor flyer

--- src/components/flyer.js
+++ src/components/flyer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Box} from '@material-ui/core';
 
 import LogoBixoquest from '../images/logo-bixoquest.png';
 
@@ -10,21 +9,23 @@ import LogoBixoquest from '../images/logo-bixoquest.png';
  */
 export default function Flyer() {
   return (
-    <Box className='flex flex-col sm:flex-row justify-center shadow-md'>
-      <Box className='flex justify-center'>
-        <img src={LogoBixoquest} alt='Logo Bixoquest' className='w-3/4'/>
-      </Box>
-      <Box className='text-center md:text-left px-2 flex flex-col sm:items-start justify-center'>
+    <div className='flex flex-col sm:flex-row justify-center shadow-md py-8'>
+      <div className='flex justify-center py-4 md:px-4'>
+        <div className='w-3/4 h-full'>
+          <img src={LogoBixoquest} alt='Logo Bixoquest'/>
+        </div>
+      </div>
+      <div className='text-center sm:text-left px-2 flex flex-col sm:items-start justify-center'>
         <h2 className='text-5xl color-black font-semibold'>A aventura vai começar!</h2>
-        <p className='color-black mb-10 px-2 md:px-0 text-lg'>
+        <p className='color-black pb-10 pt-2 px-2 sm:px-0 text-lg'>
           Já pensou em transformar a Semana de Recepção em um grande jogo?!
         </p>
         <a href='https://bixoquest.icmc.usp.br' target='_blank' rel='noopener noreferrer'>
-          <button className='bg-secondary shadow-sm mb-8 sm:mb-0 py-3 px-12 color-white rounded font-medium w-100'>
+          <button className='bg-secondary shadow-sm py-3 px-12 color-white rounded font-medium w-100'>
             Clique aqui!
           </button>
         </a>
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 }
