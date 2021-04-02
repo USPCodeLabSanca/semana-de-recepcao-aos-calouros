2b3e51adec0cb653c80a21fe630300d7f738f57c
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-02 15:09:26 -0300

    Refactor 'Contato'

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import Header from '../components/header';
+import SectionHeader from '../components/section-header';
 import Footer from '../components/footer';
-
-const style = {
-  root: 'color-black shadow-sm',
-  card: 'py-10 px-2 md:px-16',
-  title: 'text-3xl font-bold',
-};
 
 /**
  * Contato Page
@@ -15,16 +9,13 @@ const style = {
  * @return {void}
  */
 export default function Contato() {
-  return (<>
-    <Header />
-    <main className={style.root}>
-      <div className={style.card}>
-        <h1 className={style.title}>Serviço de Graduação</h1>
-        <p>Bloco 3</p>
-        <p>(16) 3373-9639</p>
-        <p>grad@icmc.usp.br</p>
-        <br/>
-      </div>
+  return (<main className='bg-primary pt-20'>
+    <SectionHeader title='Contato' />
+    <div className='container mx-auto mb-6 p-8 bg-white color-black shadow-md'>
+      <h1 className='text-xl md:text-2xl lg:text-4xl font-bold pb-4'>Serviço de Graduação</h1>
+      <p className='text-base md:text-lg lg:text-xl'>Bloco 3</p>
+      <p className='text-base md:text-lg lg:text-xl'>(16) 3373-9639</p>
+      <p className='text-base md:text-lg lg:text-xl pb-8'>grad@icmc.usp.br</p>
       <iframe
         title='Mapa para bloco 3 - serviço de graduação'
         src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2578.1624193079933!2d-47.896421983296484!3d-22.007266332805106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b877325a2f2381%3A0x7923e6d8ad682ad1!2sICMC%20-%20Instituto%20De%20Ci%C3%AAncias%20Matem%C3%A1ticas%20e%20de%20Computa%C3%A7%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1579794628095!5m2!1spt-BR!2sbr'
@@ -34,8 +25,7 @@ export default function Contato() {
         allowFullScreen
         style={{border: 0}}>
       </iframe>
-
-    </main>
+    </div>
     <Footer />
-  </>);
+  </main>);
 }
