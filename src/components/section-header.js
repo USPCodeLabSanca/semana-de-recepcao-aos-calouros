0f937f13276443dd234c212eff376622d8377c71
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-02 23:25:28 -0300

    Fix section header styles

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -8,16 +8,18 @@ import React from 'react';
 export default function SectionHeader({title, subTitle}) {
   return (
     <div className='bg-primary h-48'>
-      <div className='flex flex-col justify-center md:justify-start h-full container mx-auto px-8 md:flex-row md:items-center'>
+      <div className='flex flex-col md:flex-row  md:items-center justify-center md:justify-start h-full container mx-auto px-8'>
+        <div className='flex-grow md:flex-grow-0 h-full'></div>
         <h1 className='text-xl font-bold uppercase md:text-2xl lg:text-4xl'>{title}</h1>
         {
-          subTitle && (<div className='flex items-center pr-4 pt-8 md:py-8 md:pl-4 md:h-full'>
+          subTitle && (<div className='flex items-center pr-4 pt-8 md:py-8 md:pl-4 h-full'>
             <div className='bg-black w-1 mr-4 h-full rounded-full'></div>
             <h2 className='text-lg max-w-md md:text-xl lg:text-2xl'>
               {subTitle}
             </h2>
           </div>)
         }
+        <div className='flex-grow md:flex-grow-0 h-full'></div>
       </div>
     </div>
   );
