894377a9e6991f932041af7ce48159862df2883e
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-07 01:47:30 -0300

    Update celebration text

--- src/components/celebration.js
+++ src/components/celebration.js
@@ -12,11 +12,9 @@ export default function Celebration() {
     <div className='flex items-center bg-black min-h-screen bg-primary py-24'>
       <div className='flex flex-col items-center container mx-auto text-center px-8 text-white'>
         <div className='max-w-4xl'>
-          <p className='text-xl pt-8'>
-            Você chegou em um ano muito especial! Em 2021 <b>o ICMC está completando 50 anos</b> desde sua criação
-          </p>
           <p className='text-xl py-8'>
-            Durante o ano todo você vai poder conhecer mais sobre <b>a história e as pessoas</b> que fizeram e fazem parte dessa grande família
+            Você chegou em um ano muito especial! Em 2021 <b>o ICMC está completando 50 anos</b> desde sua criação.
+            Durante o ano todo você vai poder conhecer mais sobre <b>a história e as pessoas</b> que fizeram e fazem parte dessa grande família.
           </p>
         </div>
         <div className='flex justify-center py-8'>
