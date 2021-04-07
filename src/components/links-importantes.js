3c4b8fc2ab8cb3ad327680cc5b71f93012cc78b6
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-07 02:04:31 -0300

    Refactor 'links-importantes'

--- src/components/links-importantes.js
+++ src/components/links-importantes.js
@@ -4,45 +4,56 @@ import SectionHeader from './section-header';
 
 const links = [
   {
-    title: 'Site ICMC',
-    link: 'icmc.usp.br',
+    linkText: 'Site do ICMC',
+    link: 'https://icmc.usp.br/',
   },
   {
-    title: 'Sistema Júpiter Web',
-    link: 'uspdigital.usp.br/jupiterweb',
+    linkText: 'Manual do calouro USP',
+    link: 'https://usp.br/manualdocalouro/',
   },
   {
-    title: 'Aplicativo móvel de segurança',
-    link: 'sppu.usp.br',
+    linkText: 'Programa de Apoio à Permanência e Formação Estudantil (PAPFE)',
+    link: 'http://www.saocarlos.usp.br/calouros-com-dificuldades-socioeconomicas-podem-se-inscrever-em-programa-de-permanencia/',
   },
   {
-    title: 'FAQ e depoimentos',
-    link: 'icmc.usp.br/graduacao/faq-e-depoimentos',
+    linkText: 'Solicitação de equipamento (computador/notebook) e kit internet',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLSfrvos68papCtsSDSBWbpd7Fk7cN9SU1Wuxkt1GNgRR6P84kw/viewform',
+    text: 'Para quem precisa de recursos para acompanhar as aulas de forma remota.',
   },
   {
-    title: 'Manual do calouro USP',
-    link: 'usp.br/manualdocalouro',
+    linkText: 'Cardápio do restaurante universitário',
+    link: 'http://www.puspsc.usp.br/cardapio/',
+    text: '(O Restaurante Universitário está fazendo a entrega de marmitas para todos os alunos que estiverem ativos no Sistema USP, com o cartão físico ou com o e-card)',
+  },
+  {
+    text: 'Grupo LGBT+ do ICMC (procure no Telegram: @dan3u, @lcrodolfo, @nextlevelgih, @Ami_gayatri, @roassaf, @mcarvalhor, @allanmaniero, @arenasoy)',
   },
 ];
 
 /**
- * LinksImportantes
+ * Links Importantes
  *
  * @return {void}
  */
 export default function LinksImportantes() {
   return (
     <>
-      <SectionHeader title='Contatos' subTitle={<p>A gente pode não saber tudo, mas <b>conhecemos quem sabe</b>!</p>} />
+      <SectionHeader title='Links úteis' subTitle={<p>Referências legais para consulta!</p>} />
       <div className='bg-primary pt-4 pb-8'>
         <div className='container mx-auto px-8'>
           {links.map((link, index) => {
             return (
               <div className='py-4 text-lg sm:text-xl md:text-2xl lg:text-3xl' key={index}>
-                <h3 className='font-medium'>{link.title}</h3>
-                <a rel='noreferrer noopener' className='hover:underline break-all' href={`https://${link.link}`} target="_blank">
-                  {link.link}
-                </a>
+                {
+                  link.link &&
+                  (<a rel='noreferrer noopener' className='hover:underline break-all' href={`https://${link.link}`} target="_blank">
+                    {link.linkText}
+                  </a>)
+                }
+                {
+                  link.text &&
+                  (<p>{link.text}</p>)
+                }
               </div>
             );
           })}
