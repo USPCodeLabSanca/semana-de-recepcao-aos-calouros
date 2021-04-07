0c9c36c113d3333743819730df308e00816c3fb4
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-07 00:39:34 -0300

    Add navbar text

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -29,9 +29,19 @@ function Navbar() {
   return (<>
     <header className='bg-black shadow h-20 fixed top-0 left-0 right-0 z-10'>
       <div className='flex h-full items-center justify-between container mx-auto px-4'>
-        <Link className='h-full' to='/'>
-          <img src={Logo} alt='Logo ICMC 50 anos' className='p-4 h-full'/>
-        </Link>
+        <div className='flex items-center h-full'>
+          <Link className='h-full' to='/'>
+            <img src={Logo} alt='Logo ICMC 50 anos' className='p-4 h-full'/>
+          </Link>
+          <div className='hidden sm:block color-white py-4'>
+            <h1 className='text-xs md:text-sm lg uppercase'>
+              <b>Instituto de Ciências Matemáticas e de Computação da USP</b>
+            </h1>
+            <h2 className='text-xs'>
+              <b>semana de recepção aos calouros de 2021</b>
+            </h2>
+          </div>
+        </div>
         <div className='hidden lg:flex'>
           {links.map((link) => {
             return (<Link key={link.path} to={link.path} className='color-white p-4'><b>{link.text}</b></Link>);
