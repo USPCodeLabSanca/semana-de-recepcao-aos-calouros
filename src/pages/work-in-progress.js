a628a972b3ab8700217a79c9a696378456b43d1b
rafaeldoering <rafael.doering.soares@usp.br>
2023-03-01 22:10:05 -0300

    Página "Em Atualização"

---
+++ src/pages/work-in-progress.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * Work In Progress Page
+ *
+ * @return {void}
+ */
+export default function WorkInProgress() {
+  return (
+    <main>
+      <div className='flex items-center justify-center min-h-screen bg-secondary pt-24 md:pt-12'>
+        <h1 className='font-bold bg-yellow text-xl sm:text-2xl md:text-4xl lg:text-6xl py-4'>
+          Em atualização
+        </h1>
+      </div>
+    </main>
+  );
+}
