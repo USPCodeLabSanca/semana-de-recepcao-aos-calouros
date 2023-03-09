9de3dc0e76bce6636453ead335742e8cba0bbdfe
rafaeldoering <rafael.doering.soares@usp.br>
2023-03-08 22:26:33 -0300

    Atualiza paleta de cores

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import Logo from '../images/logo-icmc-branco.svg';
+import Logo from '../images/logo-icmc-preto.svg';
 
 /**
  * Navbar
@@ -30,7 +30,7 @@ function Navbar() {
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
 
   return (<>
-    <header className='bg-black shadow h-20 fixed top-0 left-0 right-0 z-10'>
+    <header className='bg-white color-black shadow h-20 fixed top-0 left-0 right-0 z-10'>
       <div className='flex h-full items-center justify-between container mx-auto px-4'>
         <div className='flex items-center h-full'>
           <HashLink className='h-full' to='/'>
@@ -39,11 +39,11 @@ function Navbar() {
         </div>
         <div className='hidden lg:flex'>
           {links.map((link, index) => {
-            return (<HashLink smooth key={index} to={link.path} className='color-white p-4'><b>{link.text}</b></HashLink>);
+            return (<HashLink smooth key={index} to={link.path} className='color-black p-4'><b>{link.text}</b></HashLink>);
           })}
         </div>
         <div className='lg:hidden'>
-          <IconButton color='secondary' aria-label='Abrir menu' onClick={() =>setIsDrawerOpen(true)}>
+          <IconButton color='black' aria-label='Abrir menu' onClick={() =>setIsDrawerOpen(true)}>
             <MenuIcon />
           </IconButton>
         </div>
@@ -53,14 +53,14 @@ function Navbar() {
         open={isDrawerOpen}
         onClose={() => setIsDrawerOpen(false)}
         onOpen={() => setIsDrawerOpen(true)}
-        PaperProps={{style: {backgroundColor: '#151515'}}}
+        PaperProps={{style: {backgroundColor: '#fafafa'}}}
       >
         <div
           className='flex flex-col items-center pt-4'
           style={{minWidth: '260px'}}
         >
           {links.map((link) => {
-            return (<HashLink smooth key={link.path} to={link.path} onClick={() => setIsDrawerOpen(false)} className='color-white p-4'>
+            return (<HashLink smooth key={link.path} to={link.path} onClick={() => setIsDrawerOpen(false)} className='color-black p-4'>
               <b>{link.text}</b>
             </HashLink>);
           })}
