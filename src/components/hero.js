e280bec96a80d3af4e7c1835a2e07e60feb6946b
Rebeca Carvalho <105522204+rebeca-vc@users.noreply.github.com>
2024-02-03 16:51:39 -0300

    Test workflow

--- src/components/hero.js
+++ src/components/hero.js
@@ -12,7 +12,7 @@ function Hero() {
     <div className='flex items-center min-h-screen bg-primary color-white pt-24 md:pt-12'>
       <div className='md:grid md:grid-cols-2 md:items-center container mx-auto px-8'>
         <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl'>
-          BEM-VINDOS AO INESQUECÍVEL
+          Viver a USP é melhor que sonhar!
         </h1>
         <div className='text-base sm:text-lg md:text-xl lg:text-2xl'>
           <p className='py-4'>
