35aa5b626202f0f1079c3cff8958b0e670fbe652
Rebeca <rebeca.vcarvalho78@gmail.com>
2024-02-08 20:57:13 -0300

    Update Hero main style

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-// <h1 style={{fontFamily: 'Belfast', fontSize: '6ch', fontWeight: '700'}}>Agora, <br/>nossa história é sua também.</h1>
+import Nuvem from '../images/nuvem.png';
 
 /**
  * Hero
@@ -8,20 +7,31 @@ import React from 'react';
  * @return {void}
  */
 function Hero() {
-  return (<>
-    <div className='flex items-center min-h-screen bg-primary color-white pt-24 md:pt-12'>
-      <div className='md:grid md:grid-cols-2 md:items-center container mx-auto px-8'>
-        <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl'>
-          BEM-VINDOS AO INESQUECÍVEL
-        </h1>
-        <div className='text-base sm:text-lg md:text-xl lg:text-2xl'>
-          <p className='py-4'>
-            Você acaba de ingressar em uma <b>universidade pública, gratuita e reconhecida</b> como umas das melhores do mundo.
-          </p>
+  return (
+    <>
+      <div>
+        <div className="flex items-center min-h-screen bg-primary color-white pt-24 md:pt-12">
+          <div className="md:grid md:grid-cols-2 md:items-center container mx-auto px-8">
+            <div>
+              <h1 className="text-3xl text-5xl md:text-6xl lg:text-7xl lg:pr-10 font-primary">
+                Viver a USP é melhor que sonhar.
+              </h1>
+              <div className="text-base text-xl md:text-xl lg:text-2xl">
+                <p className="py-4">
+                  Por muito tempo, você sonhou em ser aprovado na melhor
+                  universidade da América Latina.
+                </p>
+              </div>
+            </div>
+
+            <div className="text-base md:text-xl lg:text-2xl md:pl-16 pt-16 ">
+              <img src={Nuvem} alt="Nuvem de sonho" className="z-0 top-0" />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </>);
+    </>
+  );
 }
 
 export default Hero;
