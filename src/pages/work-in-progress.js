4df063ca25ad6b8cb7364f2bb091a5543382796b
shogoshima <shogoshima@usp.br>
2025-01-18 13:26:52 -0300

    adding message of work in progress

--- src/pages/work-in-progress.js
+++ src/pages/work-in-progress.js
@@ -8,9 +8,9 @@ import React from 'react';
 export default function WorkInProgress() {
   return (
     <main>
-      <div className='flex items-center justify-center min-h-screen bg-secondary pt-24 md:pt-12'>
-        <h1 className='font-bold bg-yellow text-xl sm:text-2xl md:text-4xl lg:text-6xl py-4'>
-          Em atualização
+      <div className='flex items-center justify-center bg-primary py-24 md:py-12'>
+        <h1 className='font-bold text-white text-lg sm:text-xl md:text-2xl lg:text-4xl py-4'>
+          Programação em breve!
         </h1>
       </div>
     </main>
