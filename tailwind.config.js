4bad06bf885581de55d50bac6562dc36ac2beac7
Otávio Biagioni <otaviobiagioni31@gmail.com>
2025-12-16 17:13:31 -0300

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     fontFamily: {
-      primary: ["Heading"],
-      secondary: ["Raleway"],
+      primary: ['Heading'],
+      secondary: ['Raleway'],
     },
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-
 };
