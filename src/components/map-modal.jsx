2c38366f0e32becdca2fb03d000d0af71c6f30b3
renan823 <renan.trofino@gmail.com>
2026-02-19 07:39:38 -0300

    fix modal and map size

--- src/components/map-modal.jsx
+++ src/components/map-modal.jsx
@@ -21,7 +21,7 @@ const style = {
     md: '70%',
     lg: '60%',
   },
-  height: { xs: '60vh', md: '70vh' },
+  height: { xs: '65vh', md: '70vh' },
   bgcolor: 'white',
   border: '2px solid #000',
   boxShadow: 24,
@@ -67,7 +67,7 @@ export default function MapLocationModal() {
           </IconButton>
         </Box>
         {open && (<div className='p-4'>
-          <MapContainer center={[location.lat, location.lon]} zoom={20} scrollWheelZoom={false} className="w-full h-[50vh]">
+          <MapContainer center={[location.lat, location.lon]} zoom={20} scrollWheelZoom={false} className="w-full h-[40vh]">
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
